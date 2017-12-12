979d9a6be6f6006a66fd10166bf8bb6b343eaa01
sambath <sambathkumar.rsk@gmail.com>
2017-12-11 23:51:06 -0600

    day 4

--- myFirtExpressApp/app.js
+++ myFirtExpressApp/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+// use http://localhost:3000/images/nodejs.jpg to trigger
+// http://localhost:3000/users/123456/books/100
+
+app.use(express.static('public'));
+app.get('/users/:userId/books/:bookId',  (req, res) => {
+    res.send(req.params)
+  });
+
 
 app.get('/' , (req,res) => res.send('Hello world'));
+
 app.listen(3000,() => console.log('Example app listening on port 3000'));
